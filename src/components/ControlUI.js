3e5b02db01bf5b135a0d3d526d7a50530f59e977
KeveenWong <kevwong711@gmail.com>
2023-07-27 21:45:37 -0700

    added dpad styling (looking BEAUTIFUL)

--- src/components/ControlUI.js
+++ src/components/ControlUI.js
@@ -28,40 +28,61 @@ const ControlUI = () => {
         }
     }, []);
 
-      const padStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    };
-
-    const buttonStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'gray',
-        color: 'white',
-    };
-
-    const dpadRowStyle = {
+    const padStyle = {
+      display: 'grid',
+      gridTemplateRows: 'repeat(3, 35%)',
+      gridTemplateColumns: 'repeat(3, 35%)',
+      gap: '0px', // Adjust to space buttons evenly
+      placeItems: 'center',
+      borderRadius: '5px',
+      width: '150px',
+      height: '150px',
+      margin: '50px 50px'
+  };
+  
+  const buttonStyle = {
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
-    };
+      backgroundColor: 'black',
+      color: 'violet',
+      width: '60px',
+      height: '60px',
+      borderRadius: '10px',
+      borderColor: 'violet'
+  };
 
+  const centerButtonStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    width: '70px',
+    height: '70px',
+    borderRadius: '10px',
+};
 
     return (
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <div style={{ display: 'flex' }}>
-              <div id="dpad" style={padStyle}>
+            <div id="dpad" style={padStyle}>
+              <div style={{gridRow: '1', gridColumn: '2'}}>
                   <button style={buttonStyle} onClick={() => pressKey('ArrowUp')}>&#9650;</button>
-                  <div style={dpadRowStyle}>
-                        <button style={buttonStyle} onClick={() => pressKey('ArrowLeft')}>&#9668;</button>
-                        <button style={buttonStyle} onClick={() => pressKey('ArrowRight')}>&#9658;</button>
-                  </div>
-                  <button style={buttonStyle} onClick={() => pressKey('ArrowDown')}>&#9660;</button>
-                  <div />
               </div>
+              <div style={{gridRow: '2', gridColumn: '1'}}>
+                  <button style={buttonStyle} onClick={() => pressKey('ArrowLeft')}>&#9668;</button>
+              </div>
+              <div style={{gridRow: '2', gridColumn: '3'}}>
+                  <button style={buttonStyle} onClick={() => pressKey('ArrowRight')}>&#9658;</button>
+              </div>
+              <div style={{gridRow: '3', gridColumn: '2'}}>
+                  <button style={buttonStyle} onClick={() => pressKey('ArrowDown')}>&#9660;</button>
+              </div>
+              <div style={{gridRow: '2', gridColumn: '2'}}>
+                  <div style={centerButtonStyle}></div>
+              </div>
+          </div>
               <div id="buttons" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: 20 }}>
                   <button onClick={() => handleButtonClick('z')}>A</button>
                   <button onClick={() => handleButtonClick('x')}>B</button>
